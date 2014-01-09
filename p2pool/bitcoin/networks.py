3dc8a7a791b671331318071046b81cdd27e70825
CartmanSPC <CartmanSPC@users.noreply.github.com>
2014-01-08 18:21:38 -0800

    Updated Casinocoin
    SUBSIDY_FUNC= and SANE_TARGET_RANGE= updated as reported by reductio

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -243,14 +243,14 @@ nets = dict(
             'casinocoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         )),
-        SUBSIDY_FUNC=lambda height: 50*10000000 >> (height + 1)//3153600,
+        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//3153600,
         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
         BLOCK_PERIOD=30, # s targetspacing
         SYMBOL='CSC',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'casinocoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/casinocoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.casinocoin'), 'casinocoin.conf'),
         BLOCK_EXPLORER_URL_PREFIX='http://casinocoin.mooo.com/block/',
         ADDRESS_EXPLORER_URL_PREFIX='http://casinocoin.mooo.com/address/',
-        SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=1e8,
     ),
